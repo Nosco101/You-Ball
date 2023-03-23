6183a3ae0b7e3a73a3b3bbad29ae567227d66e13
T O <tara_m_oliver@yahoo.com>
2023-03-23 17:13:23 -0400

    finished revised side menu

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // const menuBtn = document.getElementById("hamburger");
+    const menuBtn = document.getElementById("menu-icon");
 
+    const sideMenu = document.getElementById("side-menu");
+    const topbar = document.getElementById("bar1");
+    const midbar = document.getElementById("bar3");
+    const btmbar = document.getElementById("bar4");
 
+    const toggleDropdown = function () {
+      sideMenu.classList.toggle("show");
+      menuBtn.classList.toggle("hide-menu")
+      topbar.classList.toggle("top-close");
+      midbar.classList.toggle("mid-close");
+      btmbar.classList.toggle("btm-close");
+    };
+
+    menuBtn.addEventListener("click", function (e) {
+      e.stopPropagation();
+      toggleDropdown();
+    });
+
+    document.documentElement.addEventListener("click", function () {
+      if (sideMenu.classList.contains("show")) {
+        toggleDropdown();
+      }
+    });
+
+  })
